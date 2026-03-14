7d00a5b4812d02cb9659b7cc7f67a85fbc3f82b1
Kunal Pratap Singh <2005kunalpratapsingh@gmail.com>
2026-03-14 09:14:31 +0000

    i -press play , and error comes , remove this full audio feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { FirebaseClientProvider } from '@/firebase/client-provider';
-import { GlobalAudioPlayer } from '@/components/global-audio-player';
 
 export const metadata: Metadata = {
   title: 'SalusWell',
@@ -23,7 +22,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
-          <GlobalAudioPlayer />
           {children}
           <Toaster />
         </FirebaseClientProvider>
